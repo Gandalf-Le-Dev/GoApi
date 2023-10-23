85fb2b3328d89a5d939bf1c672c01bbdc2b76912
Mathis ROCHER <m.rocher@groupeonepoint.com>
2023-10-23 15:32:37 +0200

    feat: removed htmlFormatter and replaced it with html templating

--- main.go
+++ main.go
@@ -16,12 +16,14 @@ func main() {
 	r := gin.Default()
 	r.Use(cors.Default())
 	r.StaticFile("/", "./web/index.html")
+	r.LoadHTMLGlob("./web/templates/*.html")
 
 	r.GET("/posts", controllers.Posts)
 	r.GET("/posts/:id", controllers.GetPost)
+	r.GET("/posts/:id/edit", controllers.EditPost)
 	r.POST("/posts", controllers.CreatePost)
 	r.DELETE("/posts/:id", controllers.DeletePost)
 	r.PUT("/posts/:id", controllers.UpdatePost)
-	r.GET("/posts/:id/edit", controllers.EditPost)
+
 	r.Run()
 }
